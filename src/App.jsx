0855bb7f767ad9b242ff0971c82d2343efbcb744
Aman <amandhiman130@gmail.com>
2025-05-31 18:26:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,104 @@
+import { React, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import "remixicon/fonts/remixicon.css";
+const App = () => {
+  let [showContent, setShowContent] = useState(false);
+
+  useGSAP(() => {
+    const tl = gsap.timeline();
+
+    tl.to(".vi-mask-group", {
+      rotate: 10,
+      duration: 2,
+      ease: "Power4.easeInOut",
+      transformOrigin: "50% 50%",
+    }).to(".vi-mask-group", {
+      scale: 10,
+      duration: 2,
+      delay: -1.8,
+      ease: "Expo.easeInOut",
+      transformOrigin: "50% 50%",
+      opacity: 0,
+      onUpdate: function () {
+        if (this.progress() >= 0.9) {
+          document.querySelector(".svg").remove();
+          setShowContent(true);
+          this.kill();
+        }
+      },
+    });
+  });
+  return (
+    <>
+      <div className="svg flex items-center justify-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-[#000]">
+        <svg viewBox="0 0 800 600" preserveAspectRatio="xMidYMid slice">
+          <defs>
+            <mask id="viMask">
+              <rect width="100%" height="100%" fill="black" />
+              <g className="vi-mask-group">
+                <text
+                  x="50%"
+                  y="50%"
+                  fontSize="250"
+                  textAnchor="middle"
+                  fill="white"
+                  dominantBaseline="middle"
+                  fontFamily="Arial Black"
+                >
+                  VI
+                </text>
+              </g>
+            </mask>
+          </defs>
+          <image
+            href="./bg.png"
+            width="100%"
+            height="100%"
+            preserveAspectRatio="xMidYMid slice"
+            mask="url(#viMask)"
+          />
+        </svg>
+      </div>
+      {showContent && (
+        <div className="main w-full ">
+          <div className="landing w-full h-screen bg-black">
+            <div className="navbar absolute top-0 left-0 w-full z-[10] px-8 py-8 ">
+              <div className="logo flex gap-7 ">
+                <div className="lines flex flex-col gap-1">
+                  <div className="line w-14 h-2 bg-white "></div>
+                  <div className="line w-8 h-2 bg-white "></div>
+                  <div className="line w-5 h-2 bg-white "></div>
+                </div>
+                <h3 className="text-3xl text-white -mt-[8px] leading-none">
+                  Rockstar
+                </h3>
+              </div>
+            </div>
+            <div className="imagesdiv relative w-full h-screen overflow-hidden">
+              <img
+                className="absolute top-0 left-0 h-full w-full object-cover"
+                src="./sky.png"
+              ></img>
+              <img
+                className="absolute top-0 left-0 h-full w-full object-cover"
+                src="./bg.png"
+              ></img>
+              <img
+                className="absolute bottom-[-40%] left-1/2 -translate-x-1/2 scale-[0.8]"
+                src=".//girlbg.png"
+              ></img>
+            </div>
+            <div className="btmbar absolute bottom-0 left-0 w-full py-10 px-10 bg-gradient-to-t from-black to-transparent  ">
+              <div className="flelx gap-4">
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default App;
